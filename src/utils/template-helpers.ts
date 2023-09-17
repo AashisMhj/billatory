03fee50ac11584c2369e3f424779b3c1585d154b
Aashis Maharjan <aashismhj5511@gmail.com>
2023-09-17 11:20:38 +0545

    changed bill and payment slip layout

--- src/utils/template-helpers.ts
+++ src/utils/template-helpers.ts
@@ -5,23 +5,18 @@ import { convertToWords } from "@/utils/helper-function";
 
 const Bill_Layout_HEAD = `
 <head>
-    <title>Bootstrap Example</title>
+    <title>Bill Slip</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="/bootstrap.min.css" rel="stylesheet" >
+    <link rel="stylesheet" href="/bootstrap.min.css">
     <style type="text/css">
-        .logo{
-            background-size: contain;
-            background-position: center;
-            background-repeat: no-repeat;
-        }
         @media print {
             body {
                 font-size: 10px;
             }
 
             h1 {
-                font-size: 20px;
+                font-size: 16px;
             }
 
             ul.p-padding {
@@ -31,6 +26,20 @@ const Bill_Layout_HEAD = `
             div.p-row {
                 margin-top: -10px;
             }
+            ul {
+                margin: 0px;
+            }
+        }
+
+        ul {
+            margin: 0px;
+        }
+
+
+        .imagepack {
+            background-position: center 40px;
+            background-repeat: no-repeat;
+            background-size: auto 60%;
         }
     </style>
 
@@ -66,7 +75,7 @@ export function getBulkBillPageLayout() {
     `)
 }
 
-export function billFrame({ bill_items, previous_due, pan_no, phone_no, bill_no, total_sum, organization_name, month, location, student_name, roll_no, date, student_class, image }: BillProps) {
+export function billFrameV1({ bill_items, previous_due, pan_no, phone_no, bill_no, total_sum, organization_name, month, location, student_name, roll_no, date, student_class, image }: BillProps) {
     let m_bill_items = [...bill_items];
     const empty_array = Array.from({length: MAX_BILL_ITEM}, (_, k) => k);
     // if the no of items is greater than the specified then concat all the items to the last item
@@ -124,19 +133,16 @@ export function billFrame({ bill_items, previous_due, pan_no, phone_no, bill_no,
                                     <tr>
                                         <td>
                                             <ul class="list-unstyled">
-                                            ${empty_array.map((item, index) => `<li>${index < m_bill_items.length ?  m_bill_items[index].title : '&nbsp'}</li>`).join(' ')
-        }
+                                            ${empty_array.map((item, index) => `<li>${index < m_bill_items.length ?  m_bill_items[index].title : '&nbsp'}</li>`).join(' ')}
                                             </ul>
                                         <td>
                                             <ul class="list-unstyled">
-                                            ${empty_array.map((item, index) => `<li>${index < m_bill_items.length ? addComma(m_bill_items[index].amount) : '&nbsp'}</li>`).join(' ')
-        }
+                                            ${empty_array.map((item, index) => `<li>${index < m_bill_items.length ? addComma(m_bill_items[index].amount) : '&nbsp'}</li>`).join(' ')}
                                             </ul>
                                         </td>
                                         <td>
                                             <ul class="list-unstyled">
-                                            ${empty_array.map(_ => `<li></li>`).join(' ')
-        }
+                                            ${empty_array.map(_ => `<li></li>`).join(' ')}
                                             </ul>
                                         </td>
                                     </tr>
@@ -180,77 +186,110 @@ export function billFrame({ bill_items, previous_due, pan_no, phone_no, bill_no,
     `
 }
 
-
-export function paymentFrameV1({organization_name, location, pan_no, phone_no, payment_id, amount, current_date, amount_words, payee, account_name}:PaymentProps) {
+export function billFrame({ bill_items, previous_due, pan_no, phone_no, bill_no, total_sum, organization_name, month, location, student_name, roll_no, date, student_class, image }: BillProps) {
+    let m_bill_items = [...bill_items];
+    const empty_array = Array.from({length: MAX_BILL_ITEM}, (_, k) => k);
+    // if the no of items is greater than the specified then concat all the items to the last item
+    if(m_bill_items.length > MAX_BILL_ITEM){
+        const remainingItems = m_bill_items.slice(MAX_BILL_ITEM);
+        m_bill_items.splice(MAX_BILL_ITEM, m_bill_items.length -MAX_BILL_ITEM);
+        m_bill_items[MAX_BILL_ITEM] = {
+            ...m_bill_items[MAX_BILL_ITEM],
+            amount: remainingItems.reduce((prev, item)=>  prev + item.amount,0),
+            charge_title: remainingItems.map(item => item.charge_title).join('+'),
+        }
+    }
     return `
-    <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <title>Billator</title>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link href="/bootstrap.min.css" rel="stylesheet">
-            <link rel="stylesheet" href="./Diphylleia-Regular.ttf">
-            <link rel="stylesheet" href="./Sofia-Regular.ttf">
+    <div class="col-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="container">
+                        <div class="jumbotron imagepack" style="background-image: linear-gradient(rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.5)), url(${image});">
+                            <div class="row">
+                                <div class="col-6 "> PAN No.: ${pan_no} </div>
+                                <div class="col-6 text-end"> Ph No.: ${phone_no} </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="text-center">
+                                    <h1 class="mb-0">${organization_name}</h1>
+                                    <p class="fw-bold my-0">${location}</p>
+                                    <p class="my-0 text-decoration-underline "> NOTICE BILL </p>
+                                </div>
+                            </div>
+                            <div class="row p-row">
+                                <div class="col-sm-7 col-lg-9 col-7">
+                                    <ul class="list-unstyled">
+                                        <li class="text-muted">BILL No.: ${bill_no}</li>
+                                        <li class="text-muted">Name: ${student_name}</li>
+                                        <li class="text-muted">Month: ${month}</li>
+                                    </ul>
+                                </div>
+                                <div class="col-sm-5 col-lg-3 col-5 ">
+                                    <ul class="list-unstyled p-padding">
+                                        <li class="text-muted">Date: ${date}</li>
+                                        <li class="text-muted">Class: ${student_class}</li>
+                                        <li class="text-muted">Roll No.: ${roll_no}</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class=" justify-content-center">
+                            <table class="table table-bordered mb-0" style='border:1px solid black;'>
+                                <thead>
+                                    <tr>
+                                        <th class="col-9">Particulars</th>
+                                        <th class="col-2">Rs.</th>
+                                        <th class="col-1">Ps.</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <ul class="list-unstyled">
+                                            ${empty_array.map((item, index) => `<li>${index < m_bill_items.length ?  m_bill_items[index].title : '&nbsp'}</li>`).join(' ')}
+                                            </ul>
+                                        <td>
+                                            <ul class="list-unstyled">
+                                                ${empty_array.map((item, index) => `<li>${index < m_bill_items.length ? addComma(m_bill_items[index].amount) : '&nbsp'}</li>`).join(' ')}
+                                            </ul>
+                                        </td>
+                                        <td>
+                                            <ul class="list-unstyled">
+                                                ${empty_array.map(_ => `<li></li>`).join(' ')}
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-end">Total</td>
+                                        <td>${total_sum}</td>
+                                        <td></td>
+                                    </tr>
 
-        </head>
-        <body>
-            <div class="card ">
-            <div class="card-body">
-                <div class="container">
-                <h1 class="text-center" style="font-family: Sofia, sans-serif;">${organization_name}</h1>
-                
-                <div class="row ">
-                    <div class="col-3 fs-6"> PAN No.: ${pan_no} </div>
-                    <div class="fs-5 col-4 text-end fw-bold pt-2" style="font-family: Sofia, sans-serif;"> ${location} </div>
-                    <div class="col-5 text-end">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M38.3 241.3L15.1 200.6c-9.2-16.2-8.4-36.5 4.5-50C61.4 106.8 144.7 48 256 48s194.6 58.8 236.4 102.6c12.9 13.5 13.7 33.8 4.5 50l-23.1 40.7c-7.5 13.2-23.3 19.3-37.8 14.6l-81.1-26.6c-13.1-4.3-22-16.6-22-30.4V144c-49.6-18.1-104-18.1-153.6 0v54.8c0 13.8-8.9 26.1-22 30.4L76.1 255.8c-14.5 4.7-30.3-1.4-37.8-14.6zM32 336c0-8.8 7.2-16 16-16H80c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H48c-8.8 0-16-7.2-16-16V336zm0 96c0-8.8 7.2-16 16-16H80c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H48c-8.8 0-16-7.2-16-16V432zM144 320h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H144c-8.8 0-16-7.2-16-16V336c0-8.8 7.2-16 16-16zm80 16c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H240c-8.8 0-16-7.2-16-16V336zm112-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H336c-8.8 0-16-7.2-16-16V336c0-8.8 7.2-16 16-16zm80 16c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H432c-8.8 0-16-7.2-16-16V336zm16 80h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H432c-8.8 0-16-7.2-16-16V432c0-8.8 7.2-16 16-16zM128 432c0-8.8 7.2-16 16-16H368c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H144c-8.8 0-16-7.2-16-16V432z"/></svg>
-                    <span>${phone_no} </span>
+                                    <tr>
+                                        <td class="text-end">Last Dues</td>
+                                        <td>${previous_due}</td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-end">Grand Total</td>
+                                        <td>${total_sum + previous_due}</td>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class=""><strong>Amount</strong>: ${convertToWords(total_sum + previous_due)}</div>
+                        <p class="">Note: Please show in this bill should be paid within the current month by 10th. of
+                            each month</p>
                     </div>
-                </div>
-                <div class="row fs-5">
-                    <div class="col-3 mt-2 fs-6"> Receipt No.: ${payment_id}  </div>
-                    <div class="col-4 fs-5 text-end fw-bold "><span class="p-1 border border-dark">CASH RECEIPT </span> </div>
-                    <div class="col-5 fs-6 text-end mt-2">
-                    Date: ${current_date} 
-                    </div>
-                </div>
-                <div class=" justify-content-center">
-                    
-                </div>
-                </br>
-                <p class="fs-5 fw-bold" style="font-family:Diphylleia">
-                    Received with thanks from ${payee}  </br> as per the bill no ______ by Cash/Draft/Cheque No. _____________ </br>the sum of rupees ${amount_words} </br>On the account of ${account_name}
-                </p>
-            
-                
-                <div class ="row pt-4">
-                    <div class="col-3">
-                        <table class="table table-bordered   " style='border:1px solid black;border-radius:5px;'>
-                        <tr>
-                            <th class='col-1'>Rs.</th>
-                            <th class="col-2"> ${amount}</th>
-                        </tr>
-                        </table>
-                        
-                    </div>
-                    <div class="col-7 ">
-                    
-                    </div>
-                    <div class="col-2 text-end">
-                        _____________</br>
-                    <span class= "text-center">Signature</span>
-                    </div>
-                </div>
                 </div>
             </div>
-            </div>
-        </body>
-        </html>
+        </div>
     `
 }
 
-export function paymentFrame({organization_name, location, pan_no, phone_no, due_amount, payment_id, amount, current_date, amount_words, payee, account_name}:PaymentProps) {
+
+export function paymentFrame1({organization_name, location, pan_no, phone_no, due_amount, payment_id, amount, current_date, amount_words, payee, account_name}:PaymentProps) {
     return `
     <!DOCTYPE html>
 <html lang="en">
@@ -399,5 +438,136 @@ export function paymentFrame({organization_name, location, pan_no, phone_no, due
 </html>
     `
 }
+
+
+export function paymentFrame({organization_name, location, pan_no, phone_no, due_amount, payment_id, amount, current_date, amount_words, payee, account_name}:PaymentProps) {
+    return`
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Payment Slip</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/bootstrap.min.css">
+    <style>
+        tr td {
+            padding: 0 !important;
+            margin: 0 !important;
+        }
+
+        .vbottom {
+            align-items: flex-end;
+        }
+
+        div {
+            font-size: 14px;
+        }
+
+        .mg {
+            margin-top: -1rem;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="conainer-fluid m-1 p-1 mb-3 border border-dark">
+        <div class="container-fluid">
+            <div class="row d-flex">
+                <div class="col">
+                    PAN No.: ${pan_no}
+                </div>
+                <div class="col text-end">
+                    Ph No.: ${phone_no}
+                </div>
+            </div>
+            <div class="text-center mg">
+                <h4 class=" ">${organization_name}</h4>
+                <p class="fw-bold m-0">${location}</p>
+            </div>
+
+            <div class="row ">
+                <div class="col-3 col-md-3 d-flex justify-content-end fs-3 text-primary vbottom">RECEIPT</div>
+                <div class="col-4 col-md-4  d-flex justify-content-end vbottom">Date: ${current_date}</div>
+                <div class="col-5 col-md-5 d-flex justify-content-end vbottom">Receipt No.: ...........${payment_id}....</div>
+            </div>
+            <div class="row pb-1">
+                <div class="col-3 col-lg-2 d-flex justify-content-end fw-bold vbottom">Received From</div>
+                <div class="col d-flex justify-content-start vbottom">${payee}</div>
+                <div class="col  d-flex justify-content-end">
+                    <div class="row d-flex justify-content-end">
+                        <div class="col d-flex vbottom">Amount <div>
+                                <div class="col d-flex border border-dark px-2 vbottom">Rs ${amount} </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="row pb-1">
+                <div class="col-3  col-lg-2 d-flex justify-content-end ">Amount</div>
+                <div class="col d-flex justify-content-start">${convertToWords(amount)}</div>
+                <div class="col-2 d-flex justify-content-end">rupees</div>
+            </div>
+            <div class="row pb-1">
+                <div class="col-3  col-lg-2 d-flex justify-content-end fw-bold">For Payment Of</div>
+                <div class="col d-flex justify-content-start">___________________________________________________</div>
+            </div>
+
+            <div class="row pb-1">
+                
+                <div class="col-5 col-md-5 d-flex justify-content-end">
+                    <div class="row">
+                        <div class="col d-flex justify-content-end">Paid By</div>
+                        <div class="col justify-content-end">
+                            <div class="form-check ">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">Cash
+
+                            </div>
+                            <div class="form-check ">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                Cheque_____________
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">Money
+                                Order
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row pb-1">
+            <div class="col-3  col-lg-2 d-flex justify-content-end fw-bold">Received By</div>
+            <div class="col d-flex justify-content-start">___________________________________________________</div>
+
+            <div class="col-3 d-flex justify-content-end">
+                <table class="table">
+                    <tr>
+                        <td>Amount Due</td>
+                        <td>${due_amount}</td>
+                    </tr>
+                    <tr class="border-bottom border-dark">
+                        <td>This Payment</td>
+                        <td>${amount}</td>
+                    </tr>
+                    <tfoot>
+                        <tr>
+                            <td>Balance due</td>
+                            <td>${due_amount - amount}</td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+    `
+}
+
 
 
